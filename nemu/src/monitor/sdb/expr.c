2861fbb0d5829eb66f616ed26a228b066591bbd4
tracer-ics2022 <tracer@njuics.org>
2023-08-26 00:05:47 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:05:47 up 11:18,  1 user,  load average: 0.26, 0.20, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -231,18 +231,18 @@ int eval(int p, int q)
     // printf("It looks like that this expression is wrong");
     return ;
   }
-  // // else {
-  // //   printf("123");
-  // // }
-  // else if(p == q){
-  //   return atoi(tokens[p].str);
-  // } else if (check_parentheses(p, q) == true) {
-  //   /* The expression is surrounded by a matched pair of parentheses.
-  //    * If that is the case, just throw away the parentheses.
-  //    */
-  //   printf("p<q");
-  //   return eval(p + 1, q - 1);
+  // else {
+  //   printf("123");
   // }
+  else if(p == q){
+    return atoi(tokens[p].str);
+  } else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    printf("p<q");
+    return eval(p + 1, q - 1);
+  }
   // else if(p<q){
   //   printf("hh");
   //   bool flag = false;
