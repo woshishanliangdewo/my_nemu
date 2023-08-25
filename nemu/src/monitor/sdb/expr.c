b9c3d5732d5c34b7ce6de62869118919161f192c
tracer-ics2022 <tracer@njuics.org>
2023-08-25 22:53:58 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:53:58 up 10:06,  1 user,  load average: 1.12, 1.14, 1.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -144,7 +144,6 @@ static bool make_token(char *e) {
           case '+':
             tokens[nr_token].type = rules[i].token_type;
             strncpy(tokens[nr_token++].str, substr_start, substr_len);
-            printf("yes");
             break;     
           case '-':
             tokens[nr_token].type = rules[i].token_type;
