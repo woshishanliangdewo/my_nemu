f7419874a151ed1a60a29d67b80aa09d3c6c69ac
tracer-ics2022 <tracer@njuics.org>
2023-08-26 00:47:33 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:47:33 up 11:59,  1 user,  load average: 0.14, 0.20, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -247,7 +247,7 @@ int eval(int p, int q)
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
-    printf("p<q");
+    // printf("p<q");
     return eval(p + 1, q - 1);
   }
   else if(p<q){
