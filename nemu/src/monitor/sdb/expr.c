6c890305cb190026e30c66d28086f1ac8e172507
tracer-ics2022 <tracer@njuics.org>
2023-08-26 16:39:22 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:39:22 up 18:10,  1 user,  load average: 0.62, 0.24, 0.14

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -198,7 +198,7 @@ static bool make_token(char *e) {
 bool check_parentheses(int p,int q){
     printf("%d\n",p);
     printf("%d\n",q);
-    printf("%s\n",tokens[0].type);
+    printf("%s\n",tokens[1].type);
     // printf("%s\n",tokens[q].type);
     printf("%d\n",tokens[p].type == '(');
     // printf("%d\n",(tokens[p].type == '(' && tokens[q].type == ')'));
