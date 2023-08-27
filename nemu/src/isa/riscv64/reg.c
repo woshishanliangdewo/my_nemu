c270404e75d0182ca77fd2b5ff9b4a4a021e73b3
tracer-ics2022 <tracer@njuics.org>
2023-08-27 20:39:31 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:39:31 up  7:36,  1 user,  load average: 0.63, 0.47, 0.37

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -32,6 +32,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i=0;i<32;i++){
+    printf("%s",regs[i]);
     if(regs[i] == s){
       * success = true;
       return (word_t)cpu.gpr[i];
--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -397,8 +397,8 @@ int eval(int p, int q)
 
 int expr(char * e,bool *success){
 int i;
-*success = false;
 if (!make_token(e)) {
+  *success = false;
   return 0;
 }
 // /* TODO: Implement code to evaluate the expression. */
@@ -406,6 +406,7 @@ for (i=0; i<nr_token; i++){
   if(tokens[i].type == REGISTER){
       printf("%s",tokens[i].str);
       int result = isa_reg_str2val(tokens[i].str,success);
+      
       if(*success == true){
         printf("%d\n",result);
       }else {
