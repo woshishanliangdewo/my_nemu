a6f45782bb9e96d2179d41d59c8fb2a04ee61e27
tracer-ics2022 <tracer@njuics.org>
2023-08-27 09:41:21 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:41:21 up 22:22,  1 user,  load average: 0.24, 0.15, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -328,7 +328,6 @@ int eval(int p, int q)
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
-    printf("p<q");
   //   printf("%d\n",p);
   // printf("%d\n",q);
   // printf("%d\n",tokens[1].type);
@@ -346,6 +345,7 @@ int eval(int p, int q)
         }
         if(tokens[i].type == '('){
           while(tokens[i++].type != ')');
+          printf("%d",tokens[i-1].type);
           op = i;
         }
         if(!flag && (tokens[i].type == '+' || tokens[i].type == '-')){
