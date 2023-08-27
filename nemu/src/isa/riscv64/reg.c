033d88792c83f931b44a8e2294c2efa79aefba71
tracer-ics2022 <tracer@njuics.org>
2023-08-27 20:42:31 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:42:31 up  7:39,  1 user,  load average: 0.17, 0.30, 0.32

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -33,7 +33,7 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i=0;i<32;i++){
     if(regs[i] == s){
-          printf("%s",regs[i]);
+          printf("%d",i);
 
       * success = true;
       return (word_t)cpu.gpr[i];
