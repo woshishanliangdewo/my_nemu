ed700681c76958d4dc4b4c316c365de7c8abfd49
tracer-ics2022 <tracer@njuics.org>
2023-08-27 21:35:10 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:35:10 up  8:32,  1 user,  load average: 0.33, 0.25, 0.14

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -406,7 +406,7 @@ for (i=0; i<nr_token; i++){
   if(tokens[i].type == REGISTER){
       int result = isa_reg_str2val(tokens[i].str,success);
       if(*success == true){
-        strncpy(tokens[i].str,itoa(result),strlen(itoa(result)));
+          sprintf(tokens[i].str,"%d",result);
       }else {
         printf("wrong\n");
       }
