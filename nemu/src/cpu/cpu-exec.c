1e70c3ea7c0ceea299e84130455ecaf1767d3fab
tracer-ics2022 <tracer@njuics.org>
2023-08-29 00:03:58 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:03:58 up 1 day, 51 min,  1 user,  load average: 0.40, 0.15, 0.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,13 +40,13 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 // 这里就是了，g_print_step，这是一个bool，而且初始化为false
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-  int i = check_wp();
-  if(i != -1)
-  {
-    nemu_state.state = NEMU_STOP;
-    printf("你触发了监控点%d",i);
-    return ;
-  }
+  // int i = check_wp();
+  // if(i != -1)
+  // {
+  //   nemu_state.state = NEMU_STOP;
+  //   printf("你触发了监控点%d",i);
+  //   return ;
+  // }
   
 }
 static void exec_once(Decode *s, vaddr_t pc) {
