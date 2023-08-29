cc261c79c8d6a27603e5d42691948929897962ea
tracer-ics2022 <tracer@njuics.org>
2023-08-29 20:10:48 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:10:48 up 1 day,  3:09,  1 user,  load average: 0.33, 0.40, 0.40

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -474,7 +474,7 @@ int eval(int p, int q)
         continue;
       }
     }
-    printf("s%d\n",op);
+    // printf("s%d\n",op);
     // printf("%d",op);
     // printf("%d",tokens[op]);
     int val1 = eval(p, op - 1);
