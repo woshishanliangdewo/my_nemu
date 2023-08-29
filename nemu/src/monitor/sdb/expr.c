23bab5b895e1c77c4f898ab8da3d9b046a89bdfd
tracer-ics2022 <tracer@njuics.org>
2023-08-29 19:56:58 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:56:58 up 1 day,  2:55,  1 user,  load average: 0.66, 0.51, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -405,6 +405,7 @@ int eval(int p, int q)
       if(tokens[i].type == NEG)
       {
           op=max(op,i);
+          printf("fdas%d\n",op);
           i++;
           continue;
       }
@@ -474,7 +475,7 @@ int eval(int p, int q)
     // printf("%d",op);
     // printf("%d",tokens[op]);
     int val1 = eval(p, op - 1);
-    printf("fdas%d\n",op);
+    // printf("fdas%d\n",op);
     int val2 = eval(op + 1, q);
     // printf("fda%d\n",val1);
     // printf("%d",val2);
