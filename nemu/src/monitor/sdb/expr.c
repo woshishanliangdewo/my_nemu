728ca7022fd7b5ee8d0e03aee9b8a1175d10cc06
tracer-ics2022 <tracer@njuics.org>
2023-08-29 19:50:47 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:50:47 up 1 day,  2:49,  1 user,  load average: 0.66, 0.37, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -446,7 +446,9 @@ int eval(int p, int q)
 
       if (tokens[i].type == '+' || tokens[i].type == '-')
       {
+        
         if((cmp_priority(tokens[op].type,tokens[i].type) > 0) || (op==-1)){
+          printf("%d",op);
           op = max(op, i);
         }
         // printf("%d\n",tokens[i].type == '+' || tokens[i].type == '-');
