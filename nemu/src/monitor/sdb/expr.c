4fa3be5be1f0d129e6170a20531a72c77b2533ad
tracer-ics2022 <tracer@njuics.org>
2023-08-29 22:21:30 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:21:30 up 1 day,  3:21,  1 user,  load average: 0.16, 0.28, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -536,6 +536,7 @@ int expr(char *e, bool *success)
   {
     if (tokens[i].type == HEX)
     {
+      printf("%s\n",tokens[i].str);
       sprintf(tokens[i].str, "%d", strtol(tokens[i].str, NULL, 16));
       printf("%s\n",tokens[i].str);
     }
