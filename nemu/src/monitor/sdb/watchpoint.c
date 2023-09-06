4f71c4734c09ae37e652e47fedec2efe612f095f
tracer-ics2022 <tracer@njuics.org>
2023-09-07 00:53:28 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-83-generic #92~20.04.1-Ubuntu SMP Mon Aug 21 14:00:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:53:28 up  7:43,  1 user,  load average: 0.30, 0.25, 0.32

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -100,8 +100,8 @@ void create_new_wp(char *expression,int value){
     WP* wp = new_wp();
 
     // wp->expr = expression;
-    strncpy(wp->expr,expression,sizeof(expression));
-
+    // strncpy(wp->expr,expression,sizeof(expression));
+    sprintf(wp->expr,"%s",expression);
     wp->old = value;
     printf("你已经成功添加了哦\n");
 }
