ba1eb67d334a0509cef06cb701d6bea183a9696a
tracer-ics2022 <tracer@njuics.org>
2023-09-08 12:01:44 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-83-generic #92~20.04.1-Ubuntu SMP Mon Aug 21 14:00:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:01:44 up 1 day,  1:33,  1 user,  load average: 0.12, 0.19, 0.15

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -28,6 +28,7 @@
 
 #define MMIO_BASE 0xa0000000
 // 序列化端口
+// 初始是0x80000000
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
 #define KBD_ADDR        (DEVICE_BASE + 0x0000060)
 #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
