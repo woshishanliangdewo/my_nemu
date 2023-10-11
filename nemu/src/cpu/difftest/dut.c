8d512ab93f3fe345df205ce4a953f479f409b42d
tracer-ics2022 <tracer@njuics.org>
2023-10-11 10:21:51 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:21:51 up 17:18,  1 user,  load average: 0.76, 0.50, 0.45

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -33,6 +33,8 @@ static int skip_dut_nr_inst = 0;
 
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
+// 用来让difftest跳过无法与nemu保持一致的行为的
+// 确定跳过了
 void difftest_skip_ref() {
   is_skip_ref = true;
   // If such an instruction is one of the instruction packing in QEMU
@@ -42,6 +44,13 @@ void difftest_skip_ref() {
   // already write some memory, and the incoming instruction in NEMU
   // will load that memory, we will encounter false negative. But such
   // situation is infrequent.
+  //如果这样的指令是QEMU中的指令封装之一
+  //（见下文），我们结束了追赶QEMU的pc的过程
+  //保持我们一贯的最佳行为。
+  //请注意，这仍然不是完美的：如果打包的说明
+  //已经写入了一些内存，并且NEMU中的传入指令
+  //会加载那个内存，我们就会遇到假阴性。但是这样
+  //这种情况并不常见。
   skip_dut_nr_inst = 0;
 }
 
@@ -51,6 +60,13 @@ void difftest_skip_ref() {
 // The semantic is
 //   Let REF run `nr_ref` instructions first.
 //   We expect that DUT will catch up with REF within `nr_dut` instructions.
+//这用于处理QEMU中的指令打包。
+//有时让QEMU单步执行一次会执行多条指令。
+//我们应该跳过检查，直到NEMU的pc赶上QEMU的pc。
+//语义是
+//让REF先运行“nr_REF”指令。
+//我们预计DUT将在“nr_DUT”指令中赶上REF。
+// 跳过dut的指令数是nr_dut，然后我们在nr_ref指令中进行ref_dfftest_exec（1）指令
 void difftest_skip_dut(int nr_ref, int nr_dut) {
   skip_dut_nr_inst += nr_dut;
 
@@ -59,6 +75,7 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
   }
 }
 
+// 看来这里就是这些函数最后的归宿了
 void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
 
@@ -92,6 +109,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
+  // 一旦发现指令状态和pc不对，那么就变成abort态，将pc记录，并进行展示
   if (!isa_difftest_checkregs(ref, pc)) {
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
@@ -101,20 +119,28 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   CPU_state ref_r;
-
+// 如果指令数大于0
+// 那么就进行reg的regcpy
   if (skip_dut_nr_inst > 0) {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+    // 如果pc等于npc
+    // 那么跳过指令数为0
+    // 并且检查寄存器
     if (ref_r.pc == npc) {
       skip_dut_nr_inst = 0;
       checkregs(&ref_r, npc);
       return;
     }
+    // 否则就让指令一直减一
+    // 直到结束
     skip_dut_nr_inst --;
     if (skip_dut_nr_inst == 0)
       panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD, ref_r.pc, pc);
     return;
   }
-
+  // 如果是跳过指令
+  // 那么就copy这个到cpu中
+  // 并且显示跳过为false
   if (is_skip_ref) {
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
