88356a63efb7e6dd0157d5115ae7a17bb6f03fda
tracer-ics2022 <tracer@njuics.org>
2023-10-11 11:16:06 +0800

    >  compile NEMU 12020242542 刘胤宗 Linux alan-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:16:06 up 18:13,  1 user,  load average: 0.08, 0.15, 0.11

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,283 +1,274 @@
-// #include <klib.h>
-// #include <klib-macros.h>
-// #include <stdint.h>
-
-// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-// // 只是不能修改，不代表不能移动不是马
-// // strlen不包括最后的一个字符'\0'
-// size_t strlen(const char *s) {
-//   if(s == NULL)  return 0;
-//   int size = 0;
-//   while(*s != '\0'){
-//     size++;
-//     s++;
-//   }
-//   return size;
-// }
-
-// // 如果空间不够大呢
-// char *strcpy(char *dst, const char *src) {
-//   if(dst == NULL || src == NULL){
-//     return NULL;
-//   }
-//   char * tmp = dst;
-//   if((*dst = *src) != '\0')
-//   {
-//     dst++;
-//     src++;
-//   }
-//   return tmp;
-// }
-
-// // ?是否会有重叠呢
-// char *strncpy(char *dst, const char *src, size_t n) {
-//   char * tmp = dst;
-//   while(n--){
-//     *tmp = *src;
-//     tmp++;
-//     src++;
-//   }
-//   return tmp;
-//     // panic("Not implemented");
-
-//   // char *p = NULL;
-//   // if(dst == NULL || src == NULL){
-//   //   return NULL;
-//   // }
-//   // p = dst;
-//   // for(int i=0;i<n;i++){
-//   //   *dst++ = *src++;
-//   // }
-
-// }
-
-
-// char *strcat(char *dst, const char *src) {
-//     char *tmp = dst;
-//     while((*src++)!='\0'){;}
-//     *(dst++) = *(src++);
-//     return tmp;
-// }
-
-// // ？ 会有什么情况呢？
-// int strcmp(const char *s1, const char *s2) {
-//   while((s1==s2) && *s1!='\0')
-//   {
-//     s1++;
-//     s2++;
-//   }
-//   return *s2-*s1;
-//     // panic("Not implemented");
-
-// }
-
-// int strncmp(const char *s1, const char *s2, size_t n) {
-//   while((s1 == s2 )&& *s1!='\0')
-//   while(n)
-//   {
-//     s1++;
-//     s2++;
-//     n--;
-//   }
-//   return *s2-*s1;
-//   // panic("Not implemented");
-
-// }
-// void *memset(void *s, int c, size_t n) {  
-//   void * tmp = s;
-//   while(n--){
-//     *(char*)s++ = c;
-//   }
-//   return tmp;
-// }
-
-// // ？？？
-// void *memmove(void *dst, const void *src, size_t n) {
-//   char* tmp = (char*)dst;
-//   if(dst <= src || (char *)dst > (char*)src + n){
-//     while(n--)
-//     {
-//       *(char*)(dst++)=*(char*)(src++);
-//     }
-//   }
-//   else{
-//     dst = (char*)dst + n-1;
-//     src = (char *)src +n -1;
-//     while(n--){
-//     *(char *)(dst--) = *(char *)(src--);
-//     }
-//   }
-
-//   return tmp;
-//   // panic("Not implemented");
-
-// }
-
-// void *memcpy(void *out, const void *in, size_t n) {
-//   // 当我们的起始已经是所有的点的第一个的时候，+n-1就是最后一个
-//   // 只有当src加n之内是dst的时候，才会发生dst一往右移动就改变src的情况
-//   // src+n是向右的字符串，所以要小心
-//   char *tmp =(char *)out;
-//   if((char *)out < (char *)in + n){
-//       out = (char*)(out+n-1);
-//       in  = (char*)(in +n-1);
-//       while(n--){
-//          *(char*)(out--) = *(char*)(in--);
-//       }
-//   }else{
-//       *(char*)(out++) = *(char*)(in++);
-//   }
-//   return tmp;
-// }
-
-// int memcmp(const void *s1, const void *s2, size_t n) {
-//   while(n--){
-//     if(*(char*)s1 == *(char*)s2){
-//       s1++;
-//       s2++;
-//     }else{
-//       return *(char*)s1-*(char*)s2;
-//     } 
-//   }
-//   return 0;
-// }
-// #endif
-
-
-#include <klib-macros.h>
 #include <klib.h>
+#include <klib-macros.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define UCHAR_MAX (0xffU)
-
-#define SS (sizeof(size_t))
-#define ALIGN (sizeof(size_t) - 1)
-#define ONES ((size_t)-1 / UCHAR_MAX)
-#define HIGHS (ONES * (UCHAR_MAX / 2 + 1))
-#define HASZERO(x) (((x)-ONES) & ~(x) & HIGHS)
-
-size_t strlen(const char *src) {
-    int i;
-    for (i = 0; src[i] != '\0'; i++) {}
-    return i;
+// 只是不能修改，不代表不能移动不是马
+// strlen不包括最后的一个字符'\0'
+size_t strlen(const char *s) {
+  int size = 0;
+  while(*s != '\0'){
+    size++;
+    s++;
+  }
+  return size;
 }
 
-char *strcpy(char *dest, const char *src) {
-    char *tmp = dest;
+// 如果空间不够大呢
+char *strcpy(char *dst, const char *src) {
+  if(dst == NULL || src == NULL){
+    return NULL;
+  }
+  char * tmp = dst;
+  if((*dst = *src) != '\0')
+  {
+    dst++;
+    src++;
+  }
+  return tmp;
+}
 
-    while (*src) {
-        *dest++ = *src++;
-    }
+// ?是否会有重叠呢
+char *strncpy(char *dst, const char *src, size_t n) {
+  char * tmp = dst;
+  while(n--){
+    *tmp = *src;
+    tmp++;
+    src++;
+  }
+  return tmp;
+    // panic("Not implemented");
 
-    *dest = '\0';
+  // char *p = NULL;
+  // if(dst == NULL || src == NULL){
+  //   return NULL;
+  // }
+  // p = dst;
+  // for(int i=0;i<n;i++){
+  //   *dst++ = *src++;
+  // }
 
+}
+
+
+char *strcat(char *dst, const char *src) {
+    char *tmp = dst;
+    while((*src++)!='\0'){;}
+    *(dst++) = *(src++);
     return tmp;
 }
 
-char *strncpy(char *d, const char *s, size_t n) {
-    typedef size_t __attribute__((__may_alias__)) word;
-    word *wd;
-    const word *ws;
-    if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
-        for (; ((uintptr_t)s & ALIGN) && n && (*d = *s); n--, s++, d++)
-            ;
-        if (!n || !*s) {
-            goto tail;
-        }
-        wd = (void *)d;
-        ws = (const void *)s;
-        for (; n >= sizeof(size_t) && !HASZERO(*ws); n -= sizeof(size_t), ws++, wd++) {
-            *wd = *ws;
-        }
-        d = (void *)wd;
-        s = (const void *)ws;
-    }
-    for (; n && (*d = *s); n--, s++, d++)
-        ;
-tail:
-    memset(d, 0, n);
-    return d;
+// ？ 会有什么情况呢？
+int strcmp(const char *s1, const char *s2) {
+  while((s1==s2) && *s1!='\0')
+  {
+    s1++;
+    s2++;
+  }
+  return *s2-*s1;
+    // panic("Not implemented");
+
 }
 
-char *strcat(char *dest, const char *src) {
-    char *tmp = dest;
+int strncmp(const char *s1, const char *s2, size_t n) {
+  while((s1 == s2 )&& *s1!='\0')
+  while(n)
+  {
+    s1++;
+    s2++;
+    n--;
+  }
+  return *s2-*s1;
+  // panic("Not implemented");
 
-    while (*dest != '\0') {
-        dest++;
-    }
-    while (*src) {
-        *dest++ = *src++;
-    }
-
-    *dest = '\0';
-
-    return tmp;
+}
+void *memset(void *s, int c, size_t n) {  
+  void * tmp = s;
+  while(n--){
+    *(char*)s++ = c;
+  }
+  return tmp;
 }
 
-int strcmp(const char *str1, const char *str2) {
-    while (*str1 && *str2) {
-        if (*str1 != *str2) {
-            return (*str1) - (*str2);
-        }
-        ++str1;
-        ++str2;
-    }
-    return (*str1) - (*str2);
-}
-
-int strncmp(const char *_l, const char *_r, size_t n) {
-    const unsigned char *l = (void *)_l, *r = (void *)_r;
-    if (!n--) {
-        return 0;
-    }
-    for (; *l && *r && n && *l == *r; l++, r++, n--)
-        ;
-    return *l - *r;
-}
-
-void *memset(void *dest, int val, size_t len) {
-    uint8_t *dst = (uint8_t *)dest;
-
-    for (; len != 0; len--) {
-        *dst++ = val;
-    }
-
-    return dest;
-}
-
+// ？？？
 void *memmove(void *dst, const void *src, size_t n) {
-    char p_tmp[100];
-    memcpy(p_tmp, src, n);
-    memcpy(dst, p_tmp, n);
-    return dst;
+  char* tmp = (char*)dst;
+  if(dst <= src || (char *)dst > (char*)src + n){
+    while(n--)
+    {
+      *(char*)(dst++)=*(char*)(src++);
+    }
+  }
+  else{
+    dst = (char*)dst + n-1;
+    src = (char *)src +n -1;
+    while(n--){
+    *(char *)(dst--) = *(char *)(src--);
+    }
+  }
+
+  return tmp;
+  // panic("Not implemented");
+
 }
 
-void *memcpy(void *dest, const void *source, size_t len) {
-    uint64_t *dst = (uint64_t *)dest;
-    uint64_t *src = (uint64_t *)source;
-
-    for (; len >= 8;) {
-        *(uint64_t *)dst++ = *(const uint64_t *)src++;
-        len -= 8;
-    }
-    uint8_t *dst_left = (uint8_t *)dst;
-    uint8_t *src_left = (uint8_t *)src;
-    for (; len > 0; len--) {
-        *(uint8_t *)dst_left++ = *(const uint8_t *)src_left++;
-    }
-    return dest;
+void *memcpy(void *out, const void *in, size_t n) {
+  // 当我们的起始已经是所有的点的第一个的时候，+n-1就是最后一个
+  // 只有当src加n之内是dst的时候，才会发生dst一往右移动就改变src的情况
+  // src+n是向右的字符串，所以要小心
+  char *tmp =(char *)out;
+  if((char *)out < (char *)in + n){
+      out = (char*)(out+n-1);
+      in  = (char*)(in +n-1);
+      while(n--){
+         *(char*)(out--) = *(char*)(in--);
+      }
+  }else{
+      *(char*)(out++) = *(char*)(in++);
+  }
+  return tmp;
 }
 
-int memcmp(const void *ptr1, const void *ptr2, size_t num) {
-    for (int i = 0; i < num; ++i) {
-        if (((char *)ptr1)[i] != ((char *)ptr2)[i]) {
-            return ((char *)ptr1)[i] - ((char *)ptr2)[i];
-        }
-    }
-    return 0;
+int memcmp(const void *s1, const void *s2, size_t n) {
+  while(n--){
+    if(*(char*)s1 == *(char*)s2){
+      s1++;
+      s2++;
+    }else{
+      return *(char*)s1-*(char*)s2;
+    } 
+  }
+  return 0;
 }
-
 #endif
+
+
+
+
+// #define UCHAR_MAX (0xffU)
+
+// #define SS (sizeof(size_t))
+// #define ALIGN (sizeof(size_t) - 1)
+// #define ONES ((size_t)-1 / UCHAR_MAX)
+// #define HIGHS (ONES * (UCHAR_MAX / 2 + 1))
+// #define HASZERO(x) (((x)-ONES) & ~(x) & HIGHS)
+
+
+
+// char *strcpy(char *dest, const char *src) {
+//     char *tmp = dest;
+
+//     while (*src) {
+//         *dest++ = *src++;
+//     }
+
+//     *dest = '\0';
+
+//     return tmp;
+// }
+
+// char *strncpy(char *d, const char *s, size_t n) {
+//     typedef size_t __attribute__((__may_alias__)) word;
+//     word *wd;
+//     const word *ws;
+//     if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
+//         for (; ((uintptr_t)s & ALIGN) && n && (*d = *s); n--, s++, d++)
+//             ;
+//         if (!n || !*s) {
+//             goto tail;
+//         }
+//         wd = (void *)d;
+//         ws = (const void *)s;
+//         for (; n >= sizeof(size_t) && !HASZERO(*ws); n -= sizeof(size_t), ws++, wd++) {
+//             *wd = *ws;
+//         }
+//         d = (void *)wd;
+//         s = (const void *)ws;
+//     }
+//     for (; n && (*d = *s); n--, s++, d++)
+//         ;
+// tail:
+//     memset(d, 0, n);
+//     return d;
+// }
+
+// char *strcat(char *dest, const char *src) {
+//     char *tmp = dest;
+
+//     while (*dest != '\0') {
+//         dest++;
+//     }
+//     while (*src) {
+//         *dest++ = *src++;
+//     }
+
+//     *dest = '\0';
+
+//     return tmp;
+// }
+
+// int strcmp(const char *str1, const char *str2) {
+//     while (*str1 && *str2) {
+//         if (*str1 != *str2) {
+//             return (*str1) - (*str2);
+//         }
+//         ++str1;
+//         ++str2;
+//     }
+//     return (*str1) - (*str2);
+// }
+
+// int strncmp(const char *_l, const char *_r, size_t n) {
+//     const unsigned char *l = (void *)_l, *r = (void *)_r;
+//     if (!n--) {
+//         return 0;
+//     }
+//     for (; *l && *r && n && *l == *r; l++, r++, n--)
+//         ;
+//     return *l - *r;
+// }
+
+// void *memset(void *dest, int val, size_t len) {
+//     uint8_t *dst = (uint8_t *)dest;
+
+//     for (; len != 0; len--) {
+//         *dst++ = val;
+//     }
+
+//     return dest;
+// }
+
+// void *memmove(void *dst, const void *src, size_t n) {
+//     char p_tmp[100];
+//     memcpy(p_tmp, src, n);
+//     memcpy(dst, p_tmp, n);
+//     return dst;
+// }
+
+// void *memcpy(void *dest, const void *source, size_t len) {
+//     uint64_t *dst = (uint64_t *)dest;
+//     uint64_t *src = (uint64_t *)source;
+
+//     for (; len >= 8;) {
+//         *(uint64_t *)dst++ = *(const uint64_t *)src++;
+//         len -= 8;
+//     }
+//     uint8_t *dst_left = (uint8_t *)dst;
+//     uint8_t *src_left = (uint8_t *)src;
+//     for (; len > 0; len--) {
+//         *(uint8_t *)dst_left++ = *(const uint8_t *)src_left++;
+//     }
+//     return dest;
+// }
+
+// int memcmp(const void *ptr1, const void *ptr2, size_t num) {
+//     for (int i = 0; i < num; ++i) {
+//         if (((char *)ptr1)[i] != ((char *)ptr2)[i]) {
+//             return ((char *)ptr1)[i] - ((char *)ptr2)[i];
+//         }
+//     }
+//     return 0;
+// }
+
+// #endif
